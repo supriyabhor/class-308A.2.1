06875e57da23d97d8c14005d3619d39948a34e69
supriyabhor <supriyanaikwadi89@gmail.com>
2024-12-17 15:03:29 -0500

    add companion to adventurer objct

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const adventurer =
 {
     name : "Robin",
     health :10,
-    inventory : ["sword", "potion", "artifact"]
-
+    inventory : ["sword", "potion", "artifact"],
+ 
+    companion: {
+             name: "Leo",
+             type: "cat"
+             }
 }
 
 //From the adventurer object, we can access Robin’s inventory using a combination of dot notation and square bracket syntax.
@@ -14,3 +18,9 @@ for (let i=0; i< adventurer.inventory.length; i++)
 {
     console.log(adventurer.inventory[i]);
 }
+
+//Nested arrays are useful, but so are nested objects. 
+// Let’s give Robin a companion to travel with –
+//  a furry friend they call “Leo.”
+
+
