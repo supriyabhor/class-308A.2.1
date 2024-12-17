1291e1c129a89909b2f9c3d9333df176b87c72c2
supriyabhor <supriyanaikwadi89@gmail.com>
2024-12-17 14:58:32 -0500

    use for loop that logs each item in Robin’s inventory.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 
-const adventurer = {
+const adventurer = 
+{
     name : "Robin",
     health :10,
     inventory : ["sword", "potion", "artifact"]
 
+}
+
+//From the adventurer object, we can access Robin’s inventory using a combination of dot notation and square bracket syntax.
+//  For example, we could find a sword at adventurer.inventory[0].
+
+for (let i=0; i< adventurer.inventory.length; i++)
+{
+    console.log(adventurer.inventory[i]);
 }
